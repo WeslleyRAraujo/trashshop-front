d6b6b75e8219ad8d2fb26650ae98b93bbb6dcbfc
WeslleyRAraujo <weslleygithub@gmail.com>
2022-01-15 09:46:57 -0300

    trocando '' por

--- public/assets/js/app/login.js
+++ public/assets/js/app/login.js
@@ -5,7 +5,9 @@ button.onclick = () => {
     let login = document.getElementById('login').value
     
     if(isEmail(login) && password.length > 0) {
-        button.innerHTML = 'Processando  <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span><span class="visually-hidden">Loading...</span>'
+        button.innerHTML = `Processando 
+        <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+        <span class="visually-hidden">Loading...</span>`
     }
 
     function isEmail(email) {

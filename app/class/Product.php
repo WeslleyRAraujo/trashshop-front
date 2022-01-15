013478dc197072f2fca247407e34efdae56cd2d2
WeslleyRAraujo <weslleygithub@gmail.com>
2022-01-15 18:12:41 -0300

    integração de produtos com a api

--- app/class/Product.php
+++ app/class/Product.php
@@ -12,7 +12,7 @@ class Product
     {
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, self::ALL_PRODUCTS);
-        curl_setopt($curl, CURLOPT_HTTPHEADER, ['Authorization: Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJUcmFzaFNob3BJZCIsImV4cCI6MTY0NDY5MzE4NywiaWF0IjoxNjQyMjczOTg3LCJpc3MiOiJUcmFzaFNob3BJZCIsImp0aSI6IjFkOGMxZTBkLTQ0YWYtNDBlNi1iY2JlLWQ1MmM4ZjUzODY2OCIsIm5iZiI6MTY0MjI3Mzk4Niwic3ViIjozLCJ0eXAiOiJhY2Nlc3MifQ.PKFDw0GSWpsVfWEhNxkMuc2TZHBwHLg_q2SX3E7dyTNDlNAnitS8AOwkcxx_aNpIxJpojD83ehhACJVcRYaQNQ']);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, ['Authorization: Bearer ' . $_SESSION['session_token']]);
         curl_setopt($curl, CURLOPT_HEADER, 0);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
